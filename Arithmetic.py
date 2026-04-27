d228ca382937d7a8eacfe409aa1f237d4e262f82
bhargavisyamanaboina-spec <bhargavisyamanaboina@gmail.com>
2026-04-27 04:07:14 -0700

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,12 @@
+a=20
+b=5
+add=a+b
+sub=a-b
+mui=a*b
+div=a/b
+modulo=a%b
+print("The addition is:",add)
+print("The subtraction is:",sub)
+print("The multiplication is:",mul)
+print("The division is:",div)
+print("The modulus division is:",modulo)
