ff3f6d8d30471c15e2f6c817aa411630a1d440a9
bhargavisyamanaboina-spec <bhargavisyamanaboina@gmail.com>
2026-06-09 10:58:38 +0530

    Add files via upload

---
+++ 8.6.py
@@ -0,0 +1,4 @@
+import string
+text="hello,world!"
+uppercase_text=text.upper()
+print("uppercase Text:",uppercase_text)
---
+++ 8.8.py
@@ -0,0 +1,6 @@
+import platform
+system_name=platform.system()
+node_name=platform.node()
+processor_name=platform.processor()
+print("system Name:",node_name)
+print("processor Name:",processor_name)
